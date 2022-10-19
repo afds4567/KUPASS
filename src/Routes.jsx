3bc18b0d9863f63ada4dc7a3f6c3044acc4fc099
afds4567 <afds4567@gmail.com>
2022-10-19 16:15:30 +0900

    :lipstick: Feat: add login,register page UI

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ResponsiveLayout from "./layout/ResponsiveLayout";
+import Register from "./pages/Auth/Register";
+import SignIn from "./pages/Auth/SignIn";
 import PostList from "./pages/PostList/PostList";
 
 const Router = () => {
@@ -8,6 +10,8 @@ const Router = () => {
     <BrowserRouter>
       <ResponsiveLayout>
         <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/" element={<PostList />} />
         </Routes>
       </ResponsiveLayout>
